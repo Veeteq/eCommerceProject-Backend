e54a34b0b920364844dc767a51fd7feb75557efa
Veeteq <wwojnarowicz@kn.pl>
2021-12-30 11:15:07 +0100

    B001 - Integrate with Stripe (CheckoutService)

--- src/main/java/com/wojnarowicz/ecommerce/service/CheckoutService.java
+++ src/main/java/com/wojnarowicz/ecommerce/service/CheckoutService.java
@@ -1,9 +1,14 @@
 package com.wojnarowicz.ecommerce.service;
 
+import com.wojnarowicz.ecommerce.dto.PaymentInfoDTO;
 import com.wojnarowicz.ecommerce.dto.PurchaseDTO;
 import com.wojnarowicz.ecommerce.dto.PurchaseResponseDTO;
+import com.stripe.exception.StripeException
+import com.stripe.model.PaymentIntent
 
 public interface CheckoutService {
 
   public PurchaseResponseDTO checkout(PurchaseDTO purchase);
+  
+  public PaymentIntent createPaymentIntent(PaymentInfoDTO paymentInfo) throws StripeException
 }
